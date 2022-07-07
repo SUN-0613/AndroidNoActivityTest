4ea865999a2918d110331a0eab21410e7e7984ba
AkiYam <gundam.exia.vs.o.gundam@gmail.com>
2022-07-07 17:56:15 +0900

    binding検討中

--- app/src/main/java/com/noactivity/test/MainActivity.kt
+++ app/src/main/java/com/noactivity/test/MainActivity.kt
@@ -1,10 +1,17 @@
 package com.noactivity.test
 
+import androidx.activity.viewModels
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProviders
+import androidx.lifecycle.Observer
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.noactivity.test.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity()
 {
+
+    private val _viewmodel: MainViewModel by viewModels()
 
     /**
      * プログラム起動
@@ -17,6 +24,12 @@ class MainActivity : AppCompatActivity()
         // active_main.xmlを表示
         setContentView(R.layout.activity_main)
 
+        // DataBinding準備
+        var binding: ActivityMainBinding =
+            DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        // ViewModel紐づけ
+        binding.viewmodel = _viewmodel
 
 
     }
