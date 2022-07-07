42c31f43e980eb7c808566945bfd273304d0f24f
AkiYam <gundam.exia.vs.o.gundam@gmail.com>
2022-07-07 14:13:05 +0900

    Hello world表示

--- app/src/main/java/com/noactivity/test/MainActivity.kt
+++ app/src/main/java/com/noactivity/test/MainActivity.kt
@@ -1,10 +1,22 @@
 package com.noactivity.test
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity()
 {
-    
 
+    /**
+     * プログラム起動
+     */
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+
+        super.onCreate(savedInstanceState)
+
+        // active_main.xmlを表示
+        setContentView(R.layout.activity_main)
+
+    }
 
 }
