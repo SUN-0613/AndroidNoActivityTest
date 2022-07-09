2956185aedda5462c6a169b7e778b393b38b5d0d
AkiYam <gundam.exia.vs.o.gundam@gmail.com>
2022-07-09 11:07:27 +0900

    viewmodelのプロパティの値をviewに表示

--- app/src/main/java/com/noactivity/noactivitytest/MainActivity.kt
+++ app/src/main/java/com/noactivity/noactivitytest/MainActivity.kt
@@ -1,17 +1,32 @@
 package com.noactivity.noactivitytest
 
 import android.os.Bundle
-import android.os.PersistableBundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.activity.viewModels
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import com.noactivity.noactivitytest.databinding.ActivityMainBinding
+import com.noactivity.noactivitytest.viewmodel.MainViewModel
 
 class MainActivity : AppCompatActivity()
 {
+
+    private val viewModel: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.activity_main)
+        val binding: ActivityMainBinding =
+            DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.lifecycleOwner = this
+        binding.viewmodel = viewModel
+
+        //setContentView(R.layout.activity_main)
 
     }
 
